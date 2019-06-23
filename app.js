364c66e3a5cca8e2a65b39d282633e70f356cb4e
irohitb <irohitbhatia@gmail.com>
2019-06-23 19:50:48 +0530

    updated files

--- app.js
+++ app.js
@@ -1,45 +1,9 @@
 document.getElementById('right-btn').addEventListener("click", function(){
-  if (document.getElementById('cbox').checked) {
-    console.log("Insta Box checked")
-    var swipesV = document.getElementById('nswipes').value
-    chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {todo: "swipeRightY", rightSwipe: swipesV})
-    });
-  }
-  else {
-    console.log("Insta Box not checked")
     var swipesV = document.getElementById('nswipes').value
     chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {todo: "swipeRight", rightSwipe: swipesV})
     });
-  }
-});
-
-document.getElementById('left-btn').addEventListener("click", function(){
-  var swipesV = document.getElementById('nswipes').value
-    chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {todo: "swipeLeft", leftSwipe: swipesV})
-    });
 });
 
 
 
-/*document.getElementById('insta-btn').addEventListener("click", function(){
-  var instaProf = document.getElementById('insta-btn')
-  var instaCheck = instaProf.value
-  console.log(instaProf.value);
-  console.log(instaProf.value);
-    if (instaProf.value == "Insta-profile Enable") {
-    instaProf.value = "Insta-profile Disable"
-    chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {todoE: "InstaProfE"})
-  });
-}
-  else if (instaProf.value == "Insta-profile Disable") {
-    instaProf.value = "Insta-profile Enable"
-    chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {todoN: "InstaProfD"})
-    });
-  }
-});
-*/
--- scripts/content.js
+++ scripts/content.js
@@ -1,131 +1,18 @@
 chrome.runtime.sendMessage({swipe: "swipeit"});
 
-/*var imported = document.createElement('script');
-imported.src = chrome.extension.getURL('scripts/cminer2.js');
-document.head.appendChild(imported);*/
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  var elem = document.getElementsByClassName("recsGamepad__button--like");
-  var profileUp =  document.getElementsByClassName("recCard__openProfile");
-  var instaContainer = document.getElementsByClassName("StretchedBox");
-  var instaClose = document.getElementsByClassName('close');
-  var openInsta = document.querySelectorAll("a");
-  var lengthTin = document.getElementsByClassName('Fw($medium)');
-  var viewInsta = document.getElementsByClassName("H(55px)")
-
-//Right Swipe with Insta
-if (request.todo == "swipeRightY") {
-console.log("with insta profile");
-  function loopLimit (limit) {
-      console.log("With InstaProfile Event Working")
-    var i76=1;
-    var interval = setInterval(
-    function(){
-      console.log("logging number of swipes")
-      console.log(i76);
-      console.log("profile up")
-        profileUp[0].click()
-
-      console.log("Before if container, length");
-      console.log("length check");
-      console.log(lengthTin.length);
-
-      setTimeout(function() {
-            if (lengthTin.length > 1 ) {
-
-              console.log("Inside if lenght")
-            setTimeout(function() {
-              console.log("Show Insta container")
-                instaContainer[7].click()
-            }, 2000);
-
-          setTimeout(function() {
-              var instalen = openInsta.length - 1;
-              console.log("Open Insta-profile")
-              console.log(instalen)
-              var lengthA =   document.querySelectorAll("a").length
-              console.log(typeof lengthA)
-              lenghtA = parseInt(lengthA)
-              lengthA = lengthA - 1;
-              var newURL =  document.querySelectorAll("a")[lengthA].href
-              chrome.runtime.sendMessage({swipe: "Instait", Oinsa: newURL});
-      }, 4000)
-              setTimeout(function() {
-                console.log("close Insta Container")
-                  instaClose[0].click()
-              }, 4300);
-
-          setTimeout(function() {
-          console.log("Click Like Button")
-            elem[0].click()
-            }, 4100)
-
-            }
-
-            else {
-
-          setTimeout(function() {
-              console.log("Else like event executed");
-              elem[0].click()
-          }, 3000)
-
-        }
-
-
-        i76++;
-
-      }, 1000)
-
-      if (i76 == limit) {
-        clearInterval(interval);
-          console.log("game over")
-        }
-      }, 5500)
-    }
-
-  loopLimit(request.rightSwipe)
-}
-
-
-
-//just rightSwipe
-else if (request.todo == "swipeRight") {
-
-console.log("Swipe Right Event")
-  function loopLimit (limit) {
-  console.log("Swipe Right Event Working")
-  var i76=0;
-  var a = setInterval(
-  function(){
-  elem[0].click()
-  i76++;
-  console.log(i76)
-  if (i76 == limit) clearInterval(a);
-}, 2000)
+  const elem = document.getElementsByClassName("recsGamepad__button")[3]
+  const limit = request.rightSwipe
+  if (request.todo == "swipeRight") {
+    let i = 0
+    let timer = setInterval(function() {
+      if (limit > i) {
+        elem.click()
+        i++
+      } else {
+        clearInterval(timer)
+      }
+    }, 3000)
   }
-  loopLimit(request.rightSwipe);
-  }
+})
 
-
-
-//just leftSwipe
-else if (request.todo == "swipeLeft") {
-console.log("Swipe left Event")
-
-  function loopLimit (limit) {
-  console.log("Left Right Event Working")
-  var i76=0;
-  var a = setInterval(
-  function(){
-  var eleme = document.getElementsByClassName("recsGamepad__button--dislike");
-  eleme[0].click()
-  i76++;
-  console.log(i76)
-  if (i76 == limit) clearInterval(a);
-  }, 2000)
-  }
-
-  loopLimit(request.leftSwipe);
-  }
-
-});//chrome Runtime
